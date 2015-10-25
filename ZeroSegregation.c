e56ab890d10889c0ca88259a6d58249ebbe7a05b
udaykumarbhanu <udaykumarbhanu@gmail.com>
2015-10-25 16:51:19 +0530

    Zero segregation in a unsorted array with comment

--- ZeroSegregation.c
+++ ZeroSegregation.c
@@ -4,9 +4,12 @@
 void pushZeroToEnd(int a[], int n)
 {
     int count=0, i;
+    //first fill all non zero element to array.
     for(i=0; i<n; i++)
         if(a[i]!=0)
             a[count++]=a[i];
+
+    //at end insert zero to array.
     while(count<n)
         a[count++]=0;
 }
@@ -22,6 +25,7 @@ int main()
 {
     int a[]={1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
     int n=sizeof(a)/sizeof(a[0]);
+
     printf("\n Elements of array Before segregating Zero is: \n");
     printArray(a, n);
 
